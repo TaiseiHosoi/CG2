c9407030e4397393fa53450cf2d6b517c084bddd
LAPTOP-RL7QBGDD\taise <taiseigaming@gmail.com>
2022-06-29 14:21:47 +0900

    Mesh関数化

--- Mesh.cpp
+++ Mesh.cpp
@@ -34,83 +34,92 @@ Mesh::Mesh(ID3D12Device* device, XMFLOAT3 vert[]) {
 		
 	}
 
-	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
+	//頂点データ全体のサイズ
 	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
-	// 頂点バッファの設定
-	D3D12_HEAP_PROPERTIES heapProp{}; // ヒープ設定
+
+	//頂点バッファの設定
+	D3D12_HEAP_PROPERTIES heapProp{}; //ヒープ設定
 	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
-	// リソース設定
+
+	//リソース設定
 	D3D12_RESOURCE_DESC resDesc{};
 	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	resDesc.Width = sizeVB; // 頂点データ全体のサイズ
+	resDesc.Width = sizeVB; //頂点データ全体のサイズ
 	resDesc.Height = 1;
 	resDesc.DepthOrArraySize = 1;
 	resDesc.MipLevels = 1;
 	resDesc.SampleDesc.Count = 1;
 	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	// 頂点バッファの生成
+
+	//頂点バッファの生成
 	ID3D12Resource* vertBuff = nullptr;
 	result = device->CreateCommittedResource(
-		&heapProp, // ヒープ設定
+		&heapProp, //ヒープ設定
 		D3D12_HEAP_FLAG_NONE,
-		&resDesc, // リソース設定
+		&resDesc, //リソース設定
 		D3D12_RESOURCE_STATE_GENERIC_READ,
 		nullptr,
 		IID_PPV_ARGS(&vertBuff));
 	assert(SUCCEEDED(result));
 
-	// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
+	//GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
 	XMFLOAT3* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
-	// 全頂点に対して
+
+	//全頂点に対して
 	for (int i = 0; i < _countof(vertices); i++) {
-		vertMap[i] = vertices[i]; // 座標をコピー
+		vertMap[i] = vertices[i]; 
 	}
-	// 繋がりを解除
+
+	//繋がりを解除
 	vertBuff->Unmap(0, nullptr);
-	// GPU仮想アドレス
+	//GPU仮想アドレス
 	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();
-	// 頂点バッファのサイズ
+	//頂点バッファのサイズ
 	vbView.SizeInBytes = sizeVB;
-	// 頂点1つ分のデータサイズ
+	//頂点1つ分のデータサイズ
 	vbView.StrideInBytes = sizeof(XMFLOAT3);
 
-	ID3DBlob* vsBlob = nullptr; // 頂点シェーダオブジェクト
-	ID3DBlob* psBlob = nullptr; // ピクセルシェーダオブジェクト
-	ID3DBlob* errorBlob = nullptr; // エラーオブジェクト
-	// 頂点シェーダの読み込みとコンパイル
+	ID3DBlob* vsBlob = nullptr; //頂点シェーダオブジェクト
+	ID3DBlob* psBlob = nullptr; //ピクセルシェーダオブジェクト
+	ID3DBlob* errorBlob = nullptr; //エラーオブジェクト
+
+	//頂点シェーダの読み込みとコンパイル
 	result = D3DCompileFromFile(
-		L"BasicVS.hlsl", // シェーダファイル名
+		L"BasicVS.hlsl", //シェーダファイル名
 		nullptr,
-		D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
-		"main", "vs_5_0", // エントリーポイント名、シェーダーモデル指定
-		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+		D3D_COMPILE_STANDARD_FILE_INCLUDE, //インクルード可能にする
+		"main", "vs_5_0", //エントリーポイント名、シェーダーモデル指定
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, //デバッグ用設定
 		0,
 		&vsBlob, &errorBlob);
-	// エラーなら
+
+	//エラーなら
 	if (FAILED(result)) {
-		// errorBlobからエラー内容をstring型にコピー
+		//errorBlobからエラー内容をstring型にコピー
 		std::string error;
 		error.resize(errorBlob->GetBufferSize());
 		std::copy_n((char*)errorBlob->GetBufferPointer(),
 			errorBlob->GetBufferSize(),
 			error.begin());
 		error += "\n";
-		// エラー内容を出力ウィンドウに表示
+		//エラー内容を出力ウィンドウに表示
 		OutputDebugStringA(error.c_str());
 		assert(0);
 	}
-	// ピクセルシェーダの読み込みとコンパイル
+
+	//ピクセルシェーダの読み込みとコンパイル
 	result = D3DCompileFromFile(
 		L"BasicPS.hlsl", // シェーダファイル名
 		nullptr,
-		D3D_COMPILE_STANDARD_FILE_INCLUDE, // インクルード可能にする
-		"main", "ps_5_0", // エントリーポイント名、シェーダーモデル指定
-		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+		D3D_COMPILE_STANDARD_FILE_INCLUDE, //インクルード可能にする
+		"main", "ps_5_0", 
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, //デバッグ用設定
 		0,
 		&psBlob, &errorBlob);
-	// エラーなら
+
+	//エラーなら
 	if (FAILED(result)) {
 		// errorBlobからエラー内容をstring型にコピー
 		std::string error;
@@ -129,59 +138,55 @@ Mesh::Mesh(ID3D12Device* device, XMFLOAT3 vert[]) {
 	"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
 	D3D12_APPEND_ALIGNED_ELEMENT,
 	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
-	}, // (1行で書いたほうが見やすい)
+	},
 	};
 
-	// グラフィックスパイプライン設定
+	//グラフィックスパイプライン設定
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
 
-	// シェーダーの設定
+	//シェーダーの設定
 	pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
 	pipelineDesc.VS.BytecodeLength = vsBlob->GetBufferSize();
 	pipelineDesc.PS.pShaderBytecode = psBlob->GetBufferPointer();
 	pipelineDesc.PS.BytecodeLength = psBlob->GetBufferSize();
 
-	// シェーダーの設定
+	//シェーダーの設定
 	pipelineDesc.VS.pShaderBytecode = vsBlob->GetBufferPointer();
 	pipelineDesc.VS.BytecodeLength = vsBlob->GetBufferSize();
 	pipelineDesc.PS.pShaderBytecode = psBlob->GetBufferPointer();
 	pipelineDesc.PS.BytecodeLength = psBlob->GetBufferSize();
 
-	// サンプルマスクの設定
-	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK; // 標準設定
+	//サンプルマスクの設定
+	pipelineDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK; 
 
-	//////////////////////////////////////////////////////////////
-	//-------------------ラスタライザの設定------------------------//
-	/////////////////////////////////////////////////////////////
-
-	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE; // カリングしない
+	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE; 
 
 	//塗りつぶしかワイヤーのどっちかしか使えない
-	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID; // ポリゴン内塗りつぶし
-	//pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME; // ワイヤーフレーム
-	pipelineDesc.RasterizerState.DepthClipEnable = true; // 深度クリッピングを有効に
+	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID; 
+	//pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME; 
+	pipelineDesc.RasterizerState.DepthClipEnable = true; //深度クリッピングを有効に
 
-	// ブレンドステート
+	//ブレンドステート
 	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
-		= D3D12_COLOR_WRITE_ENABLE_ALL; // RBGA全てのチャンネルを描画
+		= D3D12_COLOR_WRITE_ENABLE_ALL; //RBGA全てのチャンネルを描画
 
-		//頂点レイアウトの設定
+	//頂点レイアウトの設定
 	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
 	pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
 
 	//図形の形状設定
 	pipelineDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
-	// その他の設定
-	pipelineDesc.NumRenderTargets = 1; // 描画対象は1つ
-	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; // 0~255指定のRGBA
-	pipelineDesc.SampleDesc.Count = 1; // 1ピクセルにつき1回サンプリング
+	//その他の設定
+	pipelineDesc.NumRenderTargets = 1; //描画対象は1つ
+	pipelineDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	pipelineDesc.SampleDesc.Count = 1; //1ピクセルにつき1回サンプリング
 
 
-	// ルートシグネチャの設定
+	//ルートシグネチャの設定
 	D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
 	rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-	// ルートシグネチャのシリアライズ
+	//ルートシグネチャのシリアライズ
 	ID3DBlob* rootSigBlob = nullptr;
 	result = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1_0,
 		&rootSigBlob, &errorBlob);
@@ -190,7 +195,7 @@ Mesh::Mesh(ID3D12Device* device, XMFLOAT3 vert[]) {
 		IID_PPV_ARGS(&rootSignature));
 	assert(SUCCEEDED(result));
 	rootSigBlob->Release();
-	// パイプラインにルートシグネチャをセット
+	//パイプラインにルートシグネチャをセット
 	pipelineDesc.pRootSignature = rootSignature;
 
 
@@ -202,14 +207,13 @@ Mesh::Mesh(ID3D12Device* device, XMFLOAT3 vert[]) {
 }
 
 
-
 void Mesh::draw(ID3D12GraphicsCommandList* commandList)
 {
-	//パイプライン巣チートと√シグネチャの設定コマンド
+	//パイプラインステートとルートシグネチャの設定コマンド
 	commandList->SetPipelineState(pipelineStage);
 	commandList->SetGraphicsRootSignature(rootSignature);
 
-	// プリミティブ形状の設定コマンド
+	//プリミティブ形状の設定コマンド
 	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // 三角形リスト
 
 	//頂点バッファの設定コマンド
--- Mesh.h
+++ Mesh.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<d3d12.h>
 #include<DirectXMath.h>
+
+
 class Mesh
 {
 private:
